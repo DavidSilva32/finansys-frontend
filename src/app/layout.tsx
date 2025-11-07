5f482fd469d775c7cb584f030da46111819af7c5
David Teixeira <estag_int02@controlrpa.com.br>
2025-11-07 15:07:10 -0300

    remove fonts hashed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import { ClientLanguageWrapper } from "@/components/clientLanguageWrapper";
-
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "FinanSys",
@@ -18,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className="antialiased">
         <ClientLanguageWrapper>{children}</ClientLanguageWrapper>
       </body>
     </html>
