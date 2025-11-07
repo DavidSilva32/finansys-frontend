12f606fa368917b880c24f6674a4aaac069da21c
David Teixeira <estag_int02@controlrpa.com.br>
2025-11-07 15:01:49 -0300

    Fix bug

--- src/components/protectedLayout.tsx
+++ src/components/protectedLayout.tsx
@@ -2,20 +2,22 @@
 
 import { useAuthCheck } from "@/hooks/useAuthCheck";
 import LoadingScreen from "./loadingScreen";
-import router from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const router = useRouter();
   const { isAuthenticated, isChecking } = useAuthCheck();
 
   if (isChecking) return <LoadingScreen message="Verifying session..." />;
+
   if (!isAuthenticated) {
-  router.push("/login");
-  return null;
-}
+    router.push("/login");
+    return null;
+  }
 
   return <>{children}</>;
 }
